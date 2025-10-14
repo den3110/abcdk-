4627c41b2742c5c984e23927231a01dda1996b2f
den3110 <datistpham@gmail.com>
2025-10-14 23:33:17 +0700

    commit

--- backend/rtmpRelay.server.js
+++ backend/rtmpRelay.server.js
@@ -5,7 +5,8 @@ import dotenv from "dotenv";
 import express from "express";
 import { createServer } from "http";
 import { setupStreamDashboard } from "./rtmp-service/streamDashboard.js";
-import { attachRtmpRelayUltraV2 } from "./services/rtmpRelay.js";
+import { attachRtmpRelayFinal } from "./services/rtmpRelay.js";
+// import { attachRtmpRelayUltraV2 } from "./services/rtmpRelay.js";
 dotenv.config();
 
 const PORT = process.env.RTMP_PORT || 5002;
@@ -14,7 +15,7 @@ const app = express();
 const server = createServer(app);
 
 // ✅ Start RTMP relay
-const wss = await attachRtmpRelayUltraV2(server, { path: "/ws/rtmp" });
+const wss = await attachRtmpRelayFinal(server, { path: "/ws/rtmp" });
 
 // ✅ Setup monitoring dashboard
 setupStreamDashboard(app, wss);
