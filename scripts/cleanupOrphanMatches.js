1db614452edbebadd5a75cf03558f2f098b28513
den3110 <datistpham@gmail.com>
2025-11-24 11:38:09 +0700

    commit

--- scripts/cleanupOrphanMatches.js
+++ scripts/cleanupOrphanMatches.js
@@ -4,8 +4,14 @@ import Match from "../backend/models/matchModel.js";
 import dotenv from "dotenv"
 dotenv.config()
 
+const MONGO_URI =
+  process.env.NODE_ENV === "production"
+    ? process.env.MONGO_URI_PROD || process.env.MONGODB_URI_PROD
+    : process.env.MONGO_URI || process.env.MONGODB_URI;
+
+
 async function main() {
-  await mongoose.connect(process.env.MONGO_URI);
+  await mongoose.connect(MONGO_URI);
 
   const { deletedCount } = await Match.cleanupOrphanMatches();
   console.log("DONE cleanup. Deleted:", deletedCount);
