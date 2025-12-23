b16165fac629d9166aae1d897ef4f16b01ad6e80
den3110 <datistpham@gmail.com>
2025-12-23 09:32:48 +0700

    commit

--- backend/routes/slackEventsRoutes.js
+++ backend/routes/slackEventsRoutes.js
@@ -4,10 +4,6 @@ import { slackEventsHandler } from "../controllers/slackEventsController.js";
 const router = express.Router();
 
 // QUAN TRỌNG: phải dùng raw để verify signature chuẩn
-router.post(
-  "/events",
-  express.raw({ type: "application/json" }),
-  slackEventsHandler
-);
+router.post("/events", slackEventsHandler);
 
 export default router;
